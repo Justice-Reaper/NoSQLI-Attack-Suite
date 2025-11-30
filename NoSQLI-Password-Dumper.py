0500b462e67a9b1f2d998419d4d7c1a77a611cb9
Sergio Aledo Bernal <justice.reaper.io@gmail.com>
2025-12-01 00:00:51 +0100

    Update NoSQLI-Password-Dumper.py

--- NoSQLI-Password-Dumper.py
+++ NoSQLI-Password-Dumper.py
@@ -113,7 +113,7 @@ def enumeratePasswords(url, session, users, credentials_file, password_file):
                     }
                 }
                 
-                progress_bar.status(f"{{'username': '{user}', 'password': {{'$regex': '^{password}{character}'}}}}")
+                progress_bar.status(str(payload)[-200:])
                 
                 response = make_request(session, url, payload)
                 
